31eb0857720614fd5318ea9948b6fc3924fee60f
한재훈 <sa2622@naver.com>
2021-05-10 10:23:39 +0900

    초기 커밋.

---
+++ MergeCommonForm/MergeCommonForm.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include <omp.h>
+#include <string>
+
+namespace merge {
+	class MergeCommonForm {
+	public:
+		MergeCommonForm(char* recipe, char* src, char* dest);
+		~MergeCommonForm();
+	protected:
+		void _ReadRecipe();
+		void _SaveCommonForm();
+	};
+}
+void fnMergeCommonForm()
+{
+}
